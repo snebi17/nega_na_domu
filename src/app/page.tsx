92b81eb560ed67606302a0b58547931d63578803
Nejc Šneberger <nejc.snebi@gmail.com>
2023-11-24 22:09:56 +0100

    Page under construction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center p-24">
-			<h1 className="text-4xl font-black uppercase">
+			<h1 className="text-4xl font-black uppercase text-center">
 				Stran je v izdelavi
 			</h1>
 			<small>Pridemo kmalu!</small>
